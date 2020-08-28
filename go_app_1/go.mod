b3fccefc181f12eb69dda7a22358b70a5c1fa3ba
Thomas Peters-Hall <ramius345@gmail.com>
2020-08-28 16:01:04 -0500

    do build with gin library

--- go_app_1/go.mod
+++ go_app_1/go.mod
@@ -1,3 +1,5 @@
 module github.com/ramius345/golang_jenkins_demo/go_app_1
 
 go 1.13
+
+require github.com/gin-gonic/gin v1.6.3
--- go_app_1/main.go
+++ go_app_1/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	fmt.Println("Hello")
+
+	r := gin.Default()
+	r.GET("/hello", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "hello",
+		})
+	})
+	r.Run()
 }
